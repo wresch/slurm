99e95b65d79bfa69a4d066569fb964b4c167f0b6
Danny Auble <da@schedmd.com>
2018-02-15 17:35:04 -0700

    Fix slurmdb_make_tres_string_from_simple to always print node name if nodes are given.
    Bug 2782

--- src/common/slurmdb_defs.c
+++ src/common/slurmdb_defs.c
@@ -3581,7 +3581,11 @@ extern char *slurmdb_make_tres_string_from_simple(
 				   tres_rec->name ? "/" : "",
 				   tres_rec->name ? tres_rec->name : "");
 		if (count != INFINITE64) {
-			if ((tres_rec->id == TRES_MEM) ||
+			if (nodes) {
+				node_name = find_hostname(count, nodes);
+				xstrfmtcat(tres_str, "%s", node_name);
+				xfree(node_name);
+			} else if ((tres_rec->id == TRES_MEM) ||
 			    (tres_rec->type &&
 			     !xstrcasecmp(tres_rec->type, "bb"))) {
 				char outbuf[FORMAT_STRING_SIZE];
@@ -3589,10 +3593,6 @@ extern char *slurmdb_make_tres_string_from_simple(
 						 sizeof(outbuf), UNIT_MEGA,
 						 spec_unit, convert_flags);
 				xstrfmtcat(tres_str, "%s", outbuf);
-			} else if (nodes) {
-				node_name = find_hostname(count, nodes);
-				xstrfmtcat(tres_str, "%s", node_name);
-				xfree(node_name);
 			} else {
 				xstrfmtcat(tres_str, "%"PRIu64, count);
 			}
